19d26c03542be524784250e0fdab54baa995e628
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-08 16:47:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const bgblu =()=>{
+  a.firstElementChild.style.background ="blue"
+}
+
+
+
+let a = document.body
+console.log("First child of a is :", a.firstChild)
+console.log("first element child is :",a.firstElementChild)
